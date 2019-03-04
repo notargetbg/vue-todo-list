44dae0a0790cdcdc459f43915b82648925bd5281
Zhetchko Vitchev <notargetbg@users.noreply.github.com>
2019-03-04 13:31:45 +0200

    Update static-server.js

--- server/static-server.js
+++ server/static-server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('../dist'));
+app.use(express.static(path.join(__dirname, '../dist')));
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
