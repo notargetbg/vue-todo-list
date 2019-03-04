c453db2572e8a4988d85d2702db4b9ef801ee25e
Zhetchko Vitchev <notargetbg@users.noreply.github.com>
2019-03-04 17:23:14 +0200

    Switch to pg client instead pooling because of heroku

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
-const { Pool } = require('pg');
+const { Client } = require('pg');
 
-const pool = new Pool({
+const localConf = {
 	host: 'localhost',
 	user: 'postgres',
 	password: 'root',
@@ -8,10 +8,20 @@ const pool = new Pool({
 	max: 20,
 	idleTimeoutMillis: 30000,
 	connectionTimeoutMillis: 2000,
-});
+};
+
+const prodConf = {
+	connectionString: process.env.DATABASE_URL,
+	ssl: true,
+};
+
+const config = process.env.NODE_ENV === 'production' ? prodConf : localConf;
+
+const db = new Client(config);
+db.connect();
 
 module.exports = {
 	query: (text, params, callback) => {
-		return pool.query(text, params, callback);
+		return db.query(text, params, callback);
 	}
 };
